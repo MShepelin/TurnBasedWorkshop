48a7141cf533eb5f4eb5ecbf1710f6a884efa3ed
MShepelin <shepelin.d@ya.ru>
2020-09-02 01:01:13 +0300

    Start work on UI elements

---
+++ Source/Workshop/UI/TurnBasedEvent/EventUIArrangement.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EventUIArrangement.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class WORKSHOP_API UEventUIArrangement : public UUserWidget
+{
+  GENERATED_BODY()
+};
