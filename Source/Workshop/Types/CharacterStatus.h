e1c73ffc47ec8cad2ffd46c13210a8af8d1c0ffd
MShepelin <shepelin.d@ya.ru>
2020-10-03 23:25:46 +0300

    CharacterState added

---
+++ Source/Workshop/Types/CharacterStatus.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterStatus.generated.h"
+
+
+UENUM(BlueprintType)
+enum class ECharacterStatus : uint8
+{
+  NoStatus     = 0,
+  Exhaust      = 1,  // Stamina ran out
+  Impatience   = 2,   // Patience ran out
+  Hostility    = 4,  // Amity ran out
+  Distrust     = 8,  // Trust ran out
+  Hindsight    = 16, // Sagacity ran out
+  Sorrow       = 32, // Tune ran out
+  Stupor       = 64, // Creativity ran out
+  Disrespect   = 128 // Respect ran out
+};
